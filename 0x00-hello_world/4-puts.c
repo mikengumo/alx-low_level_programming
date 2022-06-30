0e14c0959ab18403f1cfe3787682fe50916cfcd4
Michael <mikengumo@gmail.com>
2022-06-30 14:04:56 -0700

    added comments on file 4 qnd 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <string.h>
-
+/**
+ *main - Using the Puts function
+ *
+ *Return: Always 0 (Success)
+**/
 int main(void)
 {
 	char phrase[] = "\"Programming is like building a multilingual puzzle";
