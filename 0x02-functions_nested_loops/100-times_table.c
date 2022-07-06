abeec62d6b5e85a615e787d3112d617be6c293bc
Michael <mikengumo@gmail.com>
2022-07-06 12:57:50 -0700

    adjusted braces line 19 and 33

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,8 +17,8 @@ void print_times_table(int n)
 				if (j == 0)
 				{
 				_putchar(k + '0');
-
-				} else if (k < 10 && j != 0)
+				}
+				else if (k < 10 && j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
@@ -32,8 +32,8 @@ void print_times_table(int n)
 				_putchar(' ');
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
-
-				} else if (k >= 100)
+				}
+				else if (k >= 100)
 				{
 					_putchar(',');
 					_putchar(' ');
