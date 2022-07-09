644825b4461fc40163380663392107516070aa91
Michael <mikengumo@gmail.com>
2022-07-09 05:57:27 -0700

    2-10 mand tasks, modified main.h added _putchar.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,2 +1,9 @@
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+int _putchar(char);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
