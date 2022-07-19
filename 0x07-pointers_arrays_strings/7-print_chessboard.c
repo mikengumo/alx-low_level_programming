6cec4fb99e390b08034a1738572c53923e611119
Michael <mikengumo@gmail.com>
2022-07-19 10:58:32 -0700

    a function that prints the chessboard redo

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,20 +1,21 @@
-18 lines (17 sloc)  288 Bytes
-
 #include "main.h"
+
 /**
- * print_chessboard - function that prints the chessboard
- * @a: Pointer to 2-D array location
- * Return: Null
+ * print_chessboard - prints the chessboard
+ * @a: pointer to pieces to print
+ *
+ * Return: void
  */
 void print_chessboard(char (*a)[8])
 {
-	int i;
-	int j;
+	int i, j;
 
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
-			putchar(a[i][j]);
-	putchar('\n');
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
 	}
 }
