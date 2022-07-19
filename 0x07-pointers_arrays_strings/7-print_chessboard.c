7296038d8f738b9b7acac68434db8f156dd2605d
Michael <mikengumo@gmail.com>
2022-07-19 11:05:30 -0700

    attempt at printing this chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,8 +2,7 @@
 
 /**
  * print_chessboard - prints the chessboard
- * @a: pointer to pieces to print
- *
+ * @a: row
  * Return: void
  */
 void print_chessboard(char (*a)[8])
@@ -14,8 +13,13 @@ void print_chessboard(char (*a)[8])
 	{
 		for (j = 0; j < 8; j++)
 		{
-			_putchar(a[i][j]);
+			if (j == 7)
+			{
+				_putchar(a[i][j]);
+				_putchar('\n');
+			}
+			else
+				_putchar(a[i][j]);
 		}
-		_putchar('\n');
 	}
 }
